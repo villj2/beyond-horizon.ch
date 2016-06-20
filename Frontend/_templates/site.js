91a606703d4ba904b3790b2de13e003395990a44
villj2 <julien.villiger@gmail.com>
2016-06-20 21:14:07 +0200

    gallery in progress

---
+++ Frontend/_templates/site.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  
+	$('.grid').masonry({
+	  // set itemSelector so .grid-sizer is not used in layout
+	  itemSelector: '.grid-item',
+	  // use element for option
+	  columnWidth: '.grid-sizer',
+	  percentPosition: true
+	})
+
+});
