310fb8e30b77bc4a15ff32dff07b516c68ac9781
villj2 <julien.villiger@gmail.com>
2016-07-05 22:14:26 +0200

    scroll to top button

--- Frontend/_templates/site.js
+++ Frontend/_templates/site.js
@@ -212,4 +212,19 @@ $( document ).ready(function() {
 
 	    $(this).ekkoLightbox();
 	}); 
+
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},500);
+		return false;
+	});
 });
