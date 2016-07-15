72672222bf7bf75885753c664c1dce80f899d16d
villj2 <julien.villiger@gmail.com>
2016-07-15 13:48:19 +0200

    smooth scroll / gallery

--- Frontend/_templates/site.js
+++ Frontend/_templates/site.js
@@ -227,4 +227,16 @@ $( document ).ready(function() {
 		$('html, body').animate({scrollTop : 0},500);
 		return false;
 	});
+
+	$('a[href^=#]').on("click",function(e){
+	    var t= $(this.hash);
+	    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+	    if(t.length){
+	        var tOffset=t.offset().top;
+	        $('html,body').animate({scrollTop:tOffset-20},'slow');
+	        e.preventDefault();
+	    }
+	});
+
+
 });
