3504e9ead0923de27a8b1ffa5552772a56f040c9
Julien Villiger <Julien Villiger>
2016-10-08 22:01:14 +0900

    Beitrag Japan Mt Fuji

--- site/accounts/vill.php
+++ site/accounts/vill.php
@@ -9,8 +9,8 @@ password: >
 language: en
 role: admin
 history:
+  - posts/japan/fujikawaguchiko
+  - posts/japan/fujikawaguchiko/gallery
   - posts/japan/tokyo
   - posts/japan/nikko
   - posts/japan/nikko/gallery
-  - posts/japan/fujikawaguchiko
-  - posts/japan/tokyo/gallery
--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -38,7 +38,7 @@
               </div>
             </div>';
 
-            foreach($posts->sortBy('date', 'asc') as $post):
+            foreach($posts->sortBy('date', 'desc') as $post):
 
               $gallery = $post->children()->filterBy('intendedTemplate', 'postgallery');
 
