b65fbcd6e834c8b1e43ffc741097472e96237c17
villj2 <julien.villiger@gmail.com>
2019-01-16 22:29:26 +0100

    jquery updated to version 2.2.4 + map added + buildsystem updated

--- Frontend/dist/js/site.js
+++ Frontend/dist/js/site.js
@@ -252,7 +252,7 @@ $( document ).ready(function() {
 	});*/
 
 	// Smooth scrolling area-tags
-	$('area[href^=#]').on("click",function(e){
+	/*$('area[href^=#]').on("click",function(e){
 	    var t= $(this.hash);
 	    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
 	    if(t.length){
@@ -260,7 +260,7 @@ $( document ).ready(function() {
 	        $('html,body').animate({scrollTop:tOffset-140},'slow');
 	        e.preventDefault();
 	    }
-	});
+	});*/
 
 	//console.log("hihi: " + $.inArray("scrollto", getUrlVars()));
 
@@ -298,7 +298,260 @@ $( document ).ready(function() {
 		
 
 	}, 100);
+
+
+
+
+
+
+
+
+	// -------- MAP FILTER START ---------
+
+	var continentsSelected = [];
+	  var countriesSelected = [];
+
+	  var selectedList = [];
+	  /* example: 
+	  [{"id":"australia", "countries":["nz", "au"]}, {"id":"asia", "countries":["tw", "jp"]}]
+	  [{"id":"australia", "countries":["nz"]}, {"id":"asia", "countries":["jp"]}]
+	  */
+
+	  var continents = $('.continent');
+
+	  mapUpdate();
+	  listUpdate();
+	  postsUpdate();
+
+	  $(document).mousemove(function(e) {
+	    //$('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+	    //$('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+	  }).mouseover();
+
+	  var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+	  if(ios) {
+	    $('a').on('click touchend', function() { 
+	      var link = $(this).attr('href');   
+	      window.open(link,'_blank');
+	      return false;
+	    });
+	  }
+
+	  $('path').on('click', function(e) { 
+
+	    // Modify selectedList with continents
+	    mapUpdateSelectedList(e);
+	    
+	    mapUpdate();
+	    listUpdate();
+	    postsUpdate();
+
+	    return false;
+	  });
+
+	  $('#list-countries a').on('click', function(e) {
+
+	    // Modify selectedList with countries
+	    listUpdateSelectedList(e);
+
+	    listUpdate();
+	    postsUpdate();
+
+	    return false;
+	  });
+
+	  function mapUpdateSelectedList(e) {
+
+	    var continentId = $(e.target).attr('id');
+
+	    // Add or remove selected continent
+	    var indexForRemoving = -1;
+	    for(var i = 0; i<selectedList.length; i++) {
+	      var selectedObj = selectedList[i];
+
+	      // Selected continent already in list -> remove
+	      if(selectedObj.id == continentId) {
+	        indexForRemoving = i;
+	      }
+	    }
+
+	    if(indexForRemoving >= 0) {
+	      selectedList.splice(indexForRemoving, 1);
+	    }
+	    else {
+	      selectedList.push({"id":continentId, "countries":[]});
+	    }
+	  }
+
+	  function listUpdateSelectedList(e) {
+
+	    var continent = $(e.target).data("continent");
+	    var country = $(e.target).data("country");
+
+	    // Add or remove selected country
+	    for(var i = 0; i<selectedList.length; i++) {
+
+	      if(selectedList[i].id == continent){
+
+	        // Add or remove selected country
+	        var indexForRemoving = -1;
+	        for(var j = 0; j<selectedList[i].countries.length; j++) {
+
+	          // Selected continent already in list -> remove
+	          if(selectedList[i].countries[j] == country) {
+	            indexForRemoving = j;
+	          }
+	        }
+
+	        if(indexForRemoving >= 0) {
+	          selectedList[i].countries.splice(indexForRemoving, 1);
+	        }
+	        else {
+	          selectedList[i].countries.push(country);
+	        }
+	      }
+	    }
+	  }
+
+	  function mapUpdate() {
+
+	    var mapContinents = $('#continent-map .continent');
+	    
+	    for(var i = 0; i<mapContinents.length; i++) {
+
+	    	//console.log(mapContinents[i]);
+
+	      // Remove active state initially
+	      $(mapContinents[i]).removeClass('active');
+
+	      var continentId = $(mapContinents[i]).attr('id');
+
+	      // Add active state if in continentsSelected list
+	      for(var j = 0; j<selectedList.length; j++) {
+
+	        if(continentId == selectedList[j].id) {
+
+	        	console.log("add active to: " + continentId);
+
+	          $(mapContinents[i]).addClass('active');
+	          break;
+	        }
+	      }
+	    }
+	  }
+
+	  function listUpdate() {
+
+	    // initially hide all countries
+	    $('#list-countries').find('.list-country').each(function(e, target){
+	        $(target).hide();
+	    });
+
+	    // show countries based on continents in selectedList
+	    for(var i = 0; i<selectedList.length; i++) {
+
+	      var continentId = selectedList[i].id;
+
+	      var entries = $('#list-countries').children('[id^="' + continentId + '"]');
+	      entries.each(function(e, target){
+	        $(target).show();
+	      });
+	    }
+
+	    // set state of country based on country in selectedList
+	    $('#list-countries .list-country a').each(function(e, target){
+
+	      if(countryExistsInSelectedList($(target).data('country'))){
+	        $(target).addClass('active');
+	      }
+	      else{
+	        $(target).removeClass('active');
+	      }
+
+	    });
+	  }
+
+	  function postsUpdate() {
+
+	    $('#posts .posts-country').each(function(e, target){
+
+	      var continent = $(target).data('continent');
+	      var country = $(target).data('country');
+
+	      // Check if continent-goup should be active
+	      if(continentExistsInSelectedList(continent)){
+
+	        $("#posts-" + continent).show();
+
+	        if(countryExistsInSelectedList(country)){
+
+	          postsShowByCountry(country);
+	        }
+	        else {
+	          // Hide country
+	          $("#posts-" + country).hide();
+	        }
+	      }
+	      else {
+	        // Hide continent section
+	        $("#posts-" + continent).hide();
+	      }
+
+	    });
+
+	    console.log(selectedList);
+	  }
+
+	  function postsShowByCountry(country) {
+
+	    // Show country posts
+	    $("#posts-" + country).show();
+
+	    // Load pictures
+	    $("#posts-" + country).children('.posts-entry').each(function(e, target){
+	      var post = $(target);
+
+	      post.children('img').each(function(e, target){
+	        var img = $(target);
+
+	        img.attr('src', img.data('src'));
+	      });
+	    });
+
+	  }
+
+	  function continentExistsInSelectedList(continent) {
+
+	    for(var i = 0; i<selectedList.length; i++) {
+
+	      if(selectedList[i].id == continent && selectedList[i].countries.length > 0){
+	        return true;
+	      }
+	    }
+
+	    return false;
+	  }
+
+	  function countryExistsInSelectedList(country) {
+
+	    for(var i = 0; i<selectedList.length; i++) {
+
+	      for(var j = 0; j<selectedList[i].countries.length; j++) {
+
+	        if(selectedList[i].countries[j] == country) {
+
+	          return true;
+	        }
+	      }
+	    }
+
+	    return false;
+	  }
+
+	  // -------- MAP FILTER END ---------
 });
+
+// ----------------- Doc Ready END ------------------
 
 function getUrlVars()
 {
@@ -417,4 +670,17 @@ function scrollReplaceImage(Utils, imageSources, gallerySelector)
 		    //console.log('out of view');
 		}
 	})
+}
+
+Array.prototype.contains = function (element) {
+   for (i in this) {
+       if (this[i] === element) return true;
+   }
+   return false;
+}
+
+function removeFromArray(array, input) {
+  return array = jQuery.grep(array, function(value) {
+    return value != input;
+  });
 }
