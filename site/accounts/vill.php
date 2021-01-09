374d9b06e743a48de8abe76b92e25f8b9522d88d
Julien Villiger <julien.villiger@gmail.com>
2021-01-09 22:08:18 +0100

    New York post updated.

--- site/accounts/vill.php
+++ site/accounts/vill.php
@@ -9,8 +9,8 @@ password: >
 language: en
 role: admin
 history:
+  - posts/america-north/usa/new-york-city
   - posts/europe/schweden/stockholm
   - posts/europe/portugal/madeira
   - posts/europe/portugal
   - posts/europe/schweden
-  - posts/europe/portugal/madeira/gallery
